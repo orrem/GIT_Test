a54d82ee03f1cdbafad50e14667ee7d4c7ec2cad
Emelie Orraryd <emelie.orraryd@gmail.com>
2015-09-24 09:52:35 +0200

    Added a new cw for hello universe

--- Git_Test/Git_Test/Program.cs
+++ Git_Test/Git_Test/Program.cs
@@ -11,6 +11,8 @@ namespace Git_Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello Universe!");
         }
     }
 }
